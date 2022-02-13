2b31b332fc0887460a0a561129e40be95ab2be09
Sergey Sergey <918672+yeffa@users.noreply.github.com>
2022-02-13 23:46:21 +0300

    Update main.php

--- main.php
+++ main.php
@@ -1,8 +1,11 @@
 <?php
 
+// Выполняет любой таск в 10 раз бысрее
+// Загружает физически все ядра (AMD 2600 6ядер 12потоков)
+
 include 'Processes.php';
 include 'Process.php';
 
 (new Processes(
-    Process::class
-))->run(10);
+    Process::class // Имя класса процесса
+))->run('task.php', 12); // ссылка на таск, кол-во потоков
